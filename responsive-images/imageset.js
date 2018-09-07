2795102043913b008e1f2df0245c41a18de5884a
Akifumi Yamamoto <af.yamamoto@gmail.com>
2018-09-07 02:04:37 -0700

    testing with some resize algorithms

--- responsive-images/imageset.js
+++ responsive-images/imageset.js
@@ -13,6 +13,9 @@ function resize(source, filetype, newSizes) {
 	let canvas = document.createElement('canvas');
 	let sourceWidth = source.naturalWidth;
 	let sourceHeight = source.naturalHeight;
+
+	let inProcessImage = {};
+	
 	for(let i = 0; i < newSizes.length; i++) {
 		let newSize = newSizes[i];
 		if (newSize.width > sourceWidth || newSize.height > sourceHeight) {
@@ -32,7 +35,7 @@ function resize(source, filetype, newSizes) {
 }
 
 function preview(output) {
-	clearpreview();
+	clearPreview();
 	for(let i = 0; i < output.length; i++) {
 		let img = document.createElement('img');
 		img.src = output[i];
@@ -42,7 +45,7 @@ function preview(output) {
 	}
 }
 
-function clearpreview() {
+function clearPreview() {
 	let images = document.getElementsByTagName('img');
 	console.log(images);
 	for(let i = images.length-1; i >= 0; i--) {
@@ -51,4 +54,37 @@ function clearpreview() {
 			img.parentNode.removeChild(img);
 		}
 	}
+}
+
+function simpleResize(imageData, )
+
+function bicubicResize(imageData) {
+
+}
+
+// cubic interpolation based on the formulas on this page: http://www.paulinternet.nl/?page=bicubic
+function cubicInterpolation(p, x) {
+  if (Array.isArray(p) && p.length === 4) {
+    let a = -.5 * p[0] + 1.5 * p[1] - 1.5 * p[2] + .5 * p[3];
+    let b = p[0] - 2.5 * p[1] + 2 * p[2] - .5 * p[3];
+    let c = -.5 * p[0] + .5 * p[2];
+    let d = p[1];
+    
+    return a * x^3 + b * x^2 + c * x + d;
+  }
+  else {
+    throw "p is not an array";
+  }
+}
+
+function bicubicInterpolation(p, x, y) {
+  let f_p = [];
+  if (Array.isArray(p) && p.length === 4) {
+    f_p[0] = cubicInterpolation(p[0], y);
+    f_p[1] = cubicInterpolation(p[1], y);
+    f_p[2] = cubicInterpolation(p[2], y);
+    f_p[3] = cubicInterpolation(p[3], y);
+    
+    return cubicInterpolation(f_p, x);
+  }
 }
